7c5a4e83fcaa6c7f97e5a6faf874b54a90dfde72
indextrown <indextrown@gmail.com>
2025-08-30 04:10:32 +0900

    Initial Commit

---
+++ Bappuli/BappuliApp.swift
@@ -0,0 +1,17 @@
+//
+//  BappuliApp.swift
+//  Bappuli
+//
+//  Created by 김동현 on 8/30/25.
+//
+
+import SwiftUI
+
+@main
+struct BappuliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bappuli/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bappuli
+//
+//  Created by 김동현 on 8/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
